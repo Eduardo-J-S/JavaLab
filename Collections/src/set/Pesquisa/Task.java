302e2cd6379cbec76892719ba2a695bb114fa600
Eduardo-J-S <eduardojose59@hotmail.com>
2024-05-24 19:23:36 -0300

    Add: Taks Set Collections

---
+++ Collections/src/set/Pesquisa/Task.java
@@ -0,0 +1,56 @@
+package set.Pesquisa;
+
+public class Task {
+    private String description;
+    private boolean completed;
+
+    
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public boolean isCompleted() {
+        return completed;
+    }
+    
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    
+    @Override
+    public String toString() {
+        return "Task [description= " + description + ", completed= " + completed + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((description == null) ? 0 : description.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Task other = (Task) obj;
+        if (description == null) {
+            if (other.description != null)
+                return false;
+        } else if (!description.equals(other.description))
+            return false;
+        return true;
+    }
+    
+    
+}
