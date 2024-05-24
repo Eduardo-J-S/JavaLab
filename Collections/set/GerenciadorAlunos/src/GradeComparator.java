0ed89916b0761b4ce51f0778005ff6ad517bd908
Eduardo-J-S <eduardojose59@hotmail.com>
2024-05-23 23:00:54 -0300

    wastebasket: remove: Removendo Collections

--- Collections/set/GerenciadorAlunos/src/GradeComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class GradeComparator implements Comparator<Student> {
-
-    @Override
-    public int compare(Student o1, Student o2) {
-        return Double.compare(o1.getGrade(), o2.getGrade());
-    }
-}
